33dd591d3237ba89eda4ae2b36e1b6b30f9ecf4f
anamafra <--global>
2021-06-25 00:05:15 -0400

    back to top button

--- main.js
+++ main.js
@@ -54,6 +54,18 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links`,
+  #contact .text, #contact .links,
+  footer.brand, footer.social
+  `,
   { interval: 100 }
 )
+
+// BACK TO TOP BUTTON
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
